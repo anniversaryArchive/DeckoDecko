20a9543a50377381c7306c9127788932c1cf8d2d
chochozabe <durudduwls@gmail.com>
2025-10-11 15:41:53 +0900

    Refactor: Divider류 디렉토리화

--- components/index.ts
+++ components/index.ts
@@ -2,7 +2,6 @@ import Typography from "./Typography";
 import Button from "./Button";
 import { InputBox, TextBox, SearchBox } from "./Input";
 import WiggleBorder from "./WiggleBorder";
-import WiggleDivider from "./WiggleDivider";
 import ProgressBar from "./ProgressBar";
 import Chip from "./Chip";
 import GoodsThumbnail from "./GoodsThumbnail";
@@ -11,8 +10,8 @@ import { BasicSwiper, FeaturedSwiper, SimpleSwiper } from "./Swiper";
 import Segment from "./Segment";
 import DropDown from "./DropDown";
 import Icon from "./Icon";
-import Divider from "./Divider";
 import { LocalImage, NoImage } from "./Image";
+import { Divider, WiggleDivider } from "./Divider";
 
 export {
   Typography,
