7016726beb5ab1d94e416b71a4d8f8ad4b2c284c
sjkim <sojungkim318@gmail.com>
2025-10-21 09:42:47 +0900

    fix : 검색 결과 내 아이템 클릭 이벤트 연결

--- components/GoodsThumbnail.tsx
+++ components/GoodsThumbnail.tsx
@@ -1,4 +1,4 @@
-import { View, Image } from "react-native";
+import { View, Image, Pressable } from "react-native";
 import Typography from "./Typography";
 import WiggleBorder from "./WiggleBorder";
 import Button from "./Button";
@@ -7,11 +7,17 @@ type GoodsThumbnailProps = {
   nameKr: string;
   animeTitle?: string | null | undefined;
   imageLink?: string;
+  onPress?: () => void; // 클릭 이벤트 추가
 };
 
-const GoodsThumbnail = ({ nameKr, animeTitle, imageLink }: GoodsThumbnailProps) => {
+const GoodsThumbnail = ({ nameKr, animeTitle, imageLink, onPress }: GoodsThumbnailProps) => {
   return (
-    <View className="flex gap-[10px] relative">
+    <Pressable
+      onPress={onPress}
+      android_ripple={{ color: "rgba(0,0,0,0.05)" }}
+      className="relative w-[155px] mb-2"
+    >
+    {/* 이미지 영역 */}
       <View className="w-[155px] h-[155px]">
         <WiggleBorder width={155} height={155} />
         {imageLink && (
@@ -22,25 +28,33 @@ const GoodsThumbnail = ({ nameKr, animeTitle, imageLink }: GoodsThumbnailProps) 
           />
         )}
       </View>
+
       {/* 텍스트 영역 */}
-      <View className="max-w-[150px] overflow-hidden flex gap-1 pl-1">
+      <View className="max-w-[150px] overflow-hidden flex gap-1 pl-1 mt-[10px]">
         <Button
           layout="flex"
           rounded
-          onPress={() => {}}>
-          {animeTitle ? animeTitle : '기타'}
+          onPress={() => {}}
+          pointerEvents="none" // ✅ 부모 Pressable이 클릭을 받도록
+        >
+          {animeTitle ? animeTitle : "기타"}
         </Button>
+
         <Typography
           variant="Body4"
           color="primary"
           numberOfLines={2}
           ellipsizeMode="tail"
-          style={{ flexShrink: 1, minHeight: 16, textBreakStrategy: "simple" }}
+          style={{
+            flexShrink: 1,
+            minHeight: 16,
+            textBreakStrategy: "simple",
+          }}
         >
           {nameKr}
         </Typography>
       </View>
-    </View>
+    </Pressable>
   );
 };
 
