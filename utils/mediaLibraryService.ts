b3add494328dbd4ba491e45d7c6a43e893bcb511
sjkim <sojungkim318@gmail.com>
2025-12-03 19:09:25 +0900

    mediaLibraryService 주석 원복

--- utils/mediaLibraryService.ts
+++ utils/mediaLibraryService.ts
@@ -10,28 +10,34 @@ const grantedPermission = async (): Promise<boolean> => {
   try {
     const initialStatus = await MediaLibrary.getPermissionsAsync();
 
+    // 모든 권한이 허용된 경우
     if (initialStatus.granted && initialStatus.accessPrivileges === "all") {
       return true;
     }
 
+    // 권한이 허용되지 않았고, 다시 물어볼 수 있는 경우
     if (!initialStatus.granted && initialStatus.canAskAgain) {
       const { granted } = await MediaLibrary.requestPermissionsAsync();
       return granted;
     }
 
+    // '제한된 접근' 권한인 경우
     if (initialStatus.accessPrivileges === "limited") {
       linkingSettingAlert(
         "'모든 사진' 접근 허용이 필요합니다",
         "사진을 모두 보려면 설정에서 '모든 사진'으로 권한을 변경해주세요."
       );
+
       return false;
     }
 
+    // 권한이 거부되었고, 다시 물어볼 수 없는 경우
     if (!initialStatus.granted && !initialStatus.canAskAgain) {
       linkingSettingAlert(
         "권한이 거부되었습니다",
         "사진첩에 접근하려면 앱 설정에서 직접 권한을 허용해야 합니다."
       );
+
       return false;
     }
 
